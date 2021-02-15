e15c2ffeea3a7a76e349e9c37bc06d44a85d1bb4
ssarmango <ssarmango@gmail.com>
2021-02-15 12:05:51 +0900

    2. Relative, Scoll, merge

---
+++ LinearLayout-ex1/app/src/main/java/com/cos/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.cos.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity2";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+}
